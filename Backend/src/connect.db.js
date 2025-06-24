0893ac5a5ef6684cc5e75efb5bbb5b1a9b8da4a7
Tauqeer-Code-Craft <143533468+Tauqeer-Code-Craft@users.noreply.github.com>
2025-06-25 00:20:28 +0530

    Update connect.db.js

--- Backend/src/connect.db.js
+++ Backend/src/connect.db.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("DB Connected")
     } catch (error) {
         console.error(error)
